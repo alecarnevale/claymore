122b6e32641089a9ea3ba9df36f0ef737cd10a98
alecarnevale <alecarnevale.dev@gmail.com>
2022-10-28 17:26:18 +0200

    add AutobindVisitor

---
+++ lib/src/main/kotlin/com/alecarnevale/claymore/visitor/AutobindVisitor.kt
@@ -0,0 +1,55 @@
+package com.alecarnevale.claymore.visitor
+
+import com.alecarnevale.claymore.annotation.Autobind
+import com.alecarnevale.claymore.generator.ModuleWriter
+import com.alecarnevale.claymore.generator.utils.moduleClassName
+import com.alecarnevale.claymore.utils.createFile
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSType
+import com.google.devtools.ksp.symbol.KSVisitorVoid
+
+/**
+ * This visitor check if provided implementation of [Autobind] is a descendant of the annotated interface.
+ * If true it generates the necessary hilt module.
+ */
+class AutobindVisitor(
+  private val codeGenerator: CodeGenerator,
+  private val resolver: Resolver,
+  private val logger: KSPLogger
+) : KSVisitorVoid() {
+
+  override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
+    logger.info("$TAG visitClassDeclaration of $classDeclaration")
+    val arguments = classDeclaration.annotations.iterator().next().arguments
+    val ksType = arguments[0].value as KSType
+    logger.info("$TAG first arguemnt provided is $ksType")
+
+    val qualifiedName = ksType.declaration.qualifiedName
+    if (qualifiedName == null) {
+      logger.error("$TAG qualified name is null")
+      return
+    }
+
+    val classImplementationProvided = resolver.getClassDeclarationByName(qualifiedName)
+    val superTypes = classImplementationProvided?.superTypes?.map { it.resolve() }
+    if (superTypes == null) {
+      logger.error("$TAG superTypes is null")
+      return
+    }
+
+    if (classDeclaration !in superTypes.map { it.declaration }) {
+      logger.error("$TAG $classDeclaration is not a superType of $classImplementationProvided")
+      return
+    }
+
+    classDeclaration.containingFile?.let { sourceFile ->
+      val writer = ModuleWriter(classDeclaration, classImplementationProvided)
+      codeGenerator.createFile(sourceFile, writer.text(), classDeclaration.moduleClassName())
+    }
+  }
+}
+
+private const val TAG = "Claymore - AutobindVisitor:"

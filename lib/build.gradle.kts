f8128a7014c31a5badb8a5d2fb57d5e4f4637f4a
alecarnevale <alecarnevale.dev@gmail.com>
2022-11-05 08:13:23 +0100

    setup maven publish to nexus repository
    (cherry picked from commit d2e52594d8b72cee7cc0b395e9a4ba2e779d9a01)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,36 +7,93 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+  // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+  id("org.jetbrains.kotlin.jvm") version "1.6.21"
+  id("maven-publish")
+  id("signing")
 
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+  // Apply the java-library plugin for API and implementation separation.
+  `java-library`
+}
+repositories {
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+java {
+  withJavadocJar()
+  withSourcesJar()
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("nexus") {
+      groupId = "io.github.alecarnevale"
+      artifactId = "claymore"
+      version = "1.0.0"
+      from(components["java"])
+
+      repositories {
+        maven {
+          name = "Nexus"
+          url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") // Github Package
+          credentials {
+            username = properties["ossrhUsername"] as String
+            password = properties["ossrhPassword"] as String
+
+          }
+        }
+      }
+
+      pom {
+        name.set("Claymore")
+        description.set("A library that writes an hilt module for a requested interface implementation.")
+        url.set("https://github.com/alecarnevale/claymore")
+        licenses {
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+        developers {
+          developer {
+            id.set("alecarnevale")
+            name.set("Alessandro Carnevale")
+            email.set("alecarnevale.dev@gmail.com")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/alecarnevale/claymore.git")
+          developerConnection.set("scm:git:ssh://github.com/alecarnevale/claymore.git")
+          url.set("https://github.com/alecarnevale/claymore")
+        }
+      }
+    }
+  }
+}
+
+signing {
+  sign(publishing.publications["nexus"])
 }
 
 dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  // Align versions of all Kotlin components
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  // Use the Kotlin JDK 8 standard library.
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.0.1-jre")
+  // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+  implementation("com.google.guava:guava:31.0.1-jre")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+  // Use the Kotlin test library.
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+  // Use the Kotlin JUnit integration.
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+  // This dependency is exported to consumers, that is to say found on their compile classpath.
+  api("org.apache.commons:commons-math3:3.6.1")
 
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.5")
+  implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.5")
 }

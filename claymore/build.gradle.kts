23fe03f57a58b11576faf4a04d132f9b4945cef0
alecarnevale <alecarnevale.dev@gmail.com>
2022-11-05 11:29:25 +0100

    remove all unused deps

--- claymore/build.gradle.kts
+++ claymore/build.gradle.kts
@@ -63,12 +63,5 @@ signing {
 }
 
 dependencies {
-  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("com.google.guava:guava:31.0.1-jre")
-  testImplementation("org.jetbrains.kotlin:kotlin-test")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-  api("org.apache.commons:commons-math3:3.6.1")
-
   implementation("com.google.devtools.ksp:symbol-processing-api:1.7.20-1.0.8")
 }

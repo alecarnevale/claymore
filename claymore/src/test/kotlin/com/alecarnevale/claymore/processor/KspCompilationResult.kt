5bb1ccbe28cf39ad4bb69e94d41fd309fcd05bd2
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2023-04-07 17:46:47 +0200

    Add first tests (#6)
    * add jupiter, kotlin-compile-testing deps
    
    * create utils test fun
    
    * add AutobindProcessorProviderTest

---
+++ claymore/src/test/kotlin/com/alecarnevale/claymore/processor/KspCompilationResult.kt
@@ -0,0 +1,37 @@
+package com.alecarnevale.claymore.processor
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.io.File
+
+internal data class KspCompilationResult(
+  private val sourcesDir: File,
+  val result: KotlinCompilation.Result,
+) {
+  val generatedSources: List<File> get() = sourcesDir.listFilesRecursively()
+
+  private fun File.listFilesRecursively(): List<File> = listFiles().orEmpty().flatMap { file ->
+    if (file.isDirectory) file.listFilesRecursively() else listOf(file)
+  }
+
+}
+
+// because file is store in a tmp folder like /var/folders/.../ksp/sources/kotlin/
+private val File.kspSourcePath: String get() = path.substringAfter("/ksp/sources/kotlin/")
+
+internal fun KspCompilationResult.assertZeroGeneratedSources() = assertGeneratedSources()
+
+internal fun KspCompilationResult.assertGeneratedSources(vararg generatedSources: String) {
+  val sourcesPaths = this.generatedSources.map { file -> file.kspSourcePath }
+  assertEquals(generatedSources.toSet(), sourcesPaths.toSet())
+}
+
+internal fun KspCompilationResult.assertGeneratedContent(sourcePath: String, @Language("kotlin") content: String) {
+  val files = generatedSources.filter { file -> file.kspSourcePath == sourcePath }
+  return when (files.size) {
+    0 -> throw AssertionError("No files found for path $sourcePath.")
+    1 -> assertEquals(content.trimIndent(), files.first().readText())
+    else -> throw AssertionError("Multiple files found for path $sourcePath.")
+  }
+}

85b6898cf350639dea5b5fa8de9f8e2409a4f327
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2023-05-19 18:21:30 +0200

    Create new AutoBinds annotation for implementation class (#10)
    * create AutoBinds annotation
    
    * write AutoBindsVisitor
    
    * create AutoBindsProcessor
    
    * add the processor provider
    
    * write tests

---
+++ claymore/src/main/kotlin/com/alecarnevale/claymore/validator/AutoBindsValidator.kt
@@ -0,0 +1,28 @@
+package com.alecarnevale.claymore.validator
+
+import com.alecarnevale.claymore.annotation.AutoBinds
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.symbol.ClassKind
+import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.validate
+
+/**
+ * This validator check if annotated symbols is a class.
+ */
+class AutoBindsValidator(private val logger: KSPLogger) {
+  fun isValid(symbol: KSAnnotated): Boolean {
+    return symbol.validate() && symbol.isAClass()
+  }
+
+  private fun KSAnnotated.isAClass(): Boolean {
+    if (this !is KSClassDeclaration || this.classKind != ClassKind.CLASS) {
+      logger.error("$TAG ${AutoBinds::class.simpleName} annotation must annotates class")
+      return false
+    }
+
+    return true
+  }
+}
+
+private const val TAG = "Claymore - AutoBindsValidator:"
---
+++ claymore/src/test/kotlin/com/alecarnevale/claymore/processor/AutoBindsProcessorProviderTest.kt
@@ -0,0 +1,147 @@
+package com.alecarnevale.claymore.processor
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspSourcesDir
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class AutoBindsProcessorProviderTest {
+
+  @Test
+  fun `WHEN @AutoBinds is applied to a val, THEN compilation error and hilt module are not generated`() {
+    val src = SourceFile.kotlin(
+      "Foo.kt",
+      """
+      package com.example
+
+      import com.alecarnevale.claymore.annotation.AutoBinds
+      
+      @AutoBinds
+      val foo: String = "this is a variable, not a class"
+      """,
+    )
+
+    val result = compileSourceFiles(src)
+
+    assertEquals(KotlinCompilation.ExitCode.COMPILATION_ERROR, result.result.exitCode)
+    result.assertZeroGeneratedSources()
+  }
+
+  @Test
+  fun `WHEN @AutoBinds is applied to a fun, THEN compilation error and hilt module are not generated`() {
+    val src = SourceFile.kotlin(
+      "Foo.kt",
+      """
+      package com.example
+
+      import com.alecarnevale.claymore.annotation.AutoBinds
+      
+      @AutoBinds
+      fun foo() = "this is a fun, not a class"
+      """,
+    )
+
+    val result = compileSourceFiles(src)
+
+    assertEquals(KotlinCompilation.ExitCode.COMPILATION_ERROR, result.result.exitCode)
+    result.assertZeroGeneratedSources()
+  }
+
+  @Test
+  fun `WHEN @AutoBinds is applied to an interface, THEN compilation error and hilt module are not generated`() {
+    val src = SourceFile.kotlin(
+      "Foo.kt",
+      """
+      package com.example
+
+      import com.alecarnevale.claymore.annotation.AutoBinds
+      
+      @AutoBinds
+      interface Foo
+      """,
+    )
+
+    val result = compileSourceFiles(src)
+
+    assertEquals(KotlinCompilation.ExitCode.COMPILATION_ERROR, result.result.exitCode)
+    result.assertZeroGeneratedSources()
+  }
+
+  @Test
+  fun `GIVEN an interface Foo and a class Bar that doesn't implement Foo, WHEN @AutoBinds is applied to Bar, THEN compilation error and hilt module are not generated`() {
+    val src = SourceFile.kotlin(
+      "Foo.kt",
+      """
+      package com.example
+
+      import com.alecarnevale.claymore.annotation.AutoBinds
+      
+      interface Foo
+
+      @AutoBinds
+      class Bar
+      """,
+    )
+
+    val result = compileSourceFiles(src)
+
+    assertEquals(KotlinCompilation.ExitCode.COMPILATION_ERROR, result.result.exitCode)
+    result.assertZeroGeneratedSources()
+  }
+
+  @Test
+  fun `GIVEN an interface Foo and a class Bar that implements Foo, WHEN @AutoBinds is applied to Bar, THEN hilt module are generated`() {
+    val src = SourceFile.kotlin(
+      "Foo.kt",
+      """
+      package com.example
+
+      import com.alecarnevale.claymore.annotation.AutoBinds
+      
+      interface Foo
+
+      @AutoBinds
+      class Bar: Foo
+      """,
+    )
+
+    val result = compileSourceFiles(src)
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.result.exitCode)
+
+    result.assertGeneratedSources("com/example/FooModule.kt")
+    result.assertGeneratedContent(
+      "com/example/FooModule.kt",
+      """
+      package com.example
+      
+      import dagger.Binds
+      import dagger.Module
+      import dagger.hilt.InstallIn
+      import dagger.hilt.components.SingletonComponent
+      
+      @Module
+      @InstallIn(SingletonComponent::class)
+      internal interface FooModule {
+        @Binds
+        public fun foo(`impl`: Bar): Foo
+      }
+
+      """,
+    )
+  }
+
+  private fun compileSourceFiles(vararg sourceFiles: SourceFile): KspCompilationResult {
+    val kotlinCompilation = KotlinCompilation().apply {
+      sources = sourceFiles.toList()
+      symbolProcessorProviders = listOf(AutoBindsProcessorProvider())
+      inheritClassPath = true
+    }
+    return KspCompilationResult(
+      sourcesDir = kotlinCompilation.kspSourcesDir,
+      result = kotlinCompilation.compile(),
+    )
+  }
+}

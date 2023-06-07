969ac3a37e05c5daaf346600e6942da0aced0ceb
alecarnevale <alecarnevale.dev@gmail.com>
2023-06-07 20:51:55 +0200

    better tests

--- processors/src/test/kotlin/com/alecarnevale/claymore/processor/AutoBindsProcessorProviderTest.kt
+++ processors/src/test/kotlin/com/alecarnevale/claymore/processor/AutoBindsProcessorProviderTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
 class AutoBindsProcessorProviderTest {
 
   @Test
-  fun `WHEN @AutoBinds is applied to a val, THEN compilation error and hilt module are not generated`() {
+  fun `WHEN @AutoBinds is applied to a val, THEN compilation error and hilt module is not generated`() {
     val src = SourceFile.kotlin(
       "Foo.kt",
       """
@@ -31,7 +31,7 @@ class AutoBindsProcessorProviderTest {
   }
 
   @Test
-  fun `WHEN @AutoBinds is applied to a fun, THEN compilation error and hilt module are not generated`() {
+  fun `WHEN @AutoBinds is applied to a fun, THEN compilation error and hilt module is not generated`() {
     val src = SourceFile.kotlin(
       "Foo.kt",
       """
@@ -51,7 +51,7 @@ class AutoBindsProcessorProviderTest {
   }
 
   @Test
-  fun `WHEN @AutoBinds is applied to an interface, THEN compilation error and hilt module are not generated`() {
+  fun `WHEN @AutoBinds is applied to an interface, THEN compilation error and hilt module is not generated`() {
     val src = SourceFile.kotlin(
       "Foo.kt",
       """
@@ -71,7 +71,7 @@ class AutoBindsProcessorProviderTest {
   }
 
   @Test
-  fun `GIVEN an interface Foo and a class Bar that doesn't implement Foo, WHEN @AutoBinds is applied to Bar, THEN compilation error and hilt module are not generated`() {
+  fun `GIVEN an interface Foo and a class Bar that doesn't implement Foo, WHEN @AutoBinds is applied to Bar, THEN compilation error and hilt module is not generated`() {
     val src = SourceFile.kotlin(
       "Foo.kt",
       """
@@ -93,7 +93,7 @@ class AutoBindsProcessorProviderTest {
   }
 
   @Test
-  fun `GIVEN an interface Foo and a class Bar that implements Foo, WHEN @AutoBinds is applied to Bar, THEN hilt module are generated`() {
+  fun `GIVEN an interface Foo and a class Bar that implements Foo, WHEN @AutoBinds is applied to Bar, THEN hilt module is generated with SingletonComponent as default component`() {
     val src = SourceFile.kotlin(
       "Foo.kt",
       """
@@ -104,6 +104,49 @@ class AutoBindsProcessorProviderTest {
       interface Foo
 
       @AutoBinds
+      class Bar: Foo
+      """,
+    )
+
+    val result = compileSourceFiles(src)
+
+    assertEquals(KotlinCompilation.ExitCode.OK, result.result.exitCode)
+
+    result.assertGeneratedSources("com/example/FooModule.kt")
+    result.assertGeneratedContent(
+      "com/example/FooModule.kt",
+      """
+      package com.example
+      
+      import dagger.Binds
+      import dagger.Module
+      import dagger.hilt.InstallIn
+      import dagger.hilt.components.SingletonComponent
+      
+      @Module
+      @InstallIn(SingletonComponent::class)
+      internal interface FooModule {
+        @Binds
+        public fun foo(`impl`: Bar): Foo
+      }
+
+      """,
+    )
+  }
+
+  @Test
+  fun `GIVEN an interface Foo and a class Bar that implements Foo, WHEN @AutoBinds is applied to Bar with a specific component, THEN hilt module is generated using the provided component`() {
+    val src = SourceFile.kotlin(
+      "Foo.kt",
+      """
+      package com.example
+
+      import com.alecarnevale.claymore.annotations.AutoBinds
+      import dagger.hilt.components.SingletonComponent
+      
+      interface Foo
+
+      @AutoBinds(component = SingletonComponent::class)
       class Bar: Foo
       """,
     )

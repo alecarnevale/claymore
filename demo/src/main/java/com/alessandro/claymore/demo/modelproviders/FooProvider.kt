5f4189a94a9ed593240fd004a1657fe1c4fd161f
alecarnevale <alecarnevale.dev@gmail.com>
2023-06-09 07:35:24 +0200

    set the component parameter explicit

--- demo/src/main/java/com/alessandro/claymore/demo/modelproviders/FooProvider.kt
+++ demo/src/main/java/com/alessandro/claymore/demo/modelproviders/FooProvider.kt
@@ -2,8 +2,9 @@ package com.alessandro.claymore.demo.modelproviders
 
 import com.alecarnevale.claymore.annotations.InterfaceAutoBinds
 import com.alessandro.claymore.demo.models.Foo
+import dagger.hilt.android.components.ActivityComponent
 
-@InterfaceAutoBinds(implementation = FooProviderImpl::class)
+@InterfaceAutoBinds(implementation = FooProviderImpl::class, component = ActivityComponent::class)
 interface FooProvider {
   fun get(): Foo
 }

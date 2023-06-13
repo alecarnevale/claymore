a2d0a41fbc895709a251dd551d0b5bdd673499cb
alecarnevale <alecarnevale.dev@gmail.com>
2023-06-13 07:28:12 +0200

    InterfaceAutoBinds works

--- demo/app/src/main/java/com/alessandro/claymore/demo/di/FooProviderModule.kt
+++ demo/app/src/main/java/com/alessandro/claymore/demo/di/FooProviderModule.kt
@@ -3,7 +3,7 @@ package com.alessandro.claymore.demo.di
 // Not used, InterfaceAutoBinds annotation will generate this automatically ;)
 // after a successful build take a look at :demo/build/generated/ksp/debug/kotlin/com/alessandro/claymore/demo/modelproviders
 
-
+/*
 import com.alessandro.claymore.demo.modelproviders.FooProvider
 import com.alessandro.claymore.demo.modelproviders.FooProviderImpl
 import dagger.Binds
@@ -17,3 +17,4 @@ interface FooProviderModule {
   @Binds
   fun fooProvider(impl: FooProviderImpl): FooProvider
 }
+*/

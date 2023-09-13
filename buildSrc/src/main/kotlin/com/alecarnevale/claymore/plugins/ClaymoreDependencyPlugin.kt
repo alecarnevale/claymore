1bb5681fcf7516ecb70986c35ad3963c758be7e4
alecarnevale <alecarnevale.dev@gmail.com>
2023-09-13 18:02:50 +0200

    add log inside ClaymoreDependencyPlugin

--- buildSrc/src/main/kotlin/com/alecarnevale/claymore/plugins/ClaymoreDependencyPlugin.kt
+++ buildSrc/src/main/kotlin/com/alecarnevale/claymore/plugins/ClaymoreDependencyPlugin.kt
@@ -2,15 +2,18 @@ package com.alecarnevale.claymore.plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
 
 internal class ClaymoreDependencyPlugin: Plugin<Project> {
   override fun apply(project: Project) {
     val fetchProp = project.properties.containsKey("fetchRemoteDependency")
 
     if (fetchProp) {
+      project.logger.log(LogLevel.LIFECYCLE, "You are using remote claymore version")
       project.dependencies.add("compileOnly", project.libs("claymore.annotation"))
       project.dependencies.add("ksp", project.libs("claymore.processors"))
     } else {
+      project.logger.log(LogLevel.LIFECYCLE, "You are using local claymore version")
       project.dependencies.add("compileOnly", project.project(":annotations"))
       project.dependencies.add("ksp", project.project(":processors"))
     }

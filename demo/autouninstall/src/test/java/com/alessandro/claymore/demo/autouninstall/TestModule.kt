a3ba9f16b81aea7b1fdf6cd338fb9c7072f390b8
alecarnevale <alecarnevale.dev@gmail.com>
2024-01-16 19:07:54 +0100

    apply AutoUninstall annotation

--- demo/autouninstall/src/test/java/com/alessandro/claymore/demo/autouninstall/TestModule.kt
+++ demo/autouninstall/src/test/java/com/alessandro/claymore/demo/autouninstall/TestModule.kt
@@ -1,19 +1,18 @@
 package com.alessandro.claymore.demo.autouninstall
 
+import com.alecarnevale.claymore.annotations.AutoUninstall
 import com.alessandro.claymore.demo.autouninstall.models.OtherService
-import com.alessandro.claymore.demo.autouninstall.models.OtherServiceImplModule
+import com.alessandro.claymore.demo.autouninstall.models.OtherServiceImpl
 import com.alessandro.claymore.demo.autouninstall.models.Service
-import com.alessandro.claymore.demo.autouninstall.models.ServiceImplModule
+import com.alessandro.claymore.demo.autouninstall.models.ServiceImpl
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dagger.hilt.testing.TestInstallIn
 
 @Module
-@TestInstallIn(
-  components = [SingletonComponent::class],
-  replaces = [ServiceImplModule::class, OtherServiceImplModule::class]
-)
+@InstallIn(SingletonComponent::class)
+@AutoUninstall(implementations = [ServiceImpl::class, OtherServiceImpl::class])
 internal object TestModule {
 
   @get:Provides

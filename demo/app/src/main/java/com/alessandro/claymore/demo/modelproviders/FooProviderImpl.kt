7d6d1a5bac8478f797ff5e512b953454f7116869
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-01-24 18:08:25 +0100

    Remove InterfaceAutoBinds annotation (#88)
    * delete InterfaceAutoBinds annotation
    
    * delete InterfaceAutoBindsProcessor
    
    * update AutoUninstall doc
    
    * remove InterfaceAutoBinds from demo project

--- demo/app/src/main/java/com/alessandro/claymore/demo/modelproviders/FooProviderImpl.kt
+++ demo/app/src/main/java/com/alessandro/claymore/demo/modelproviders/FooProviderImpl.kt
@@ -1,9 +1,12 @@
 package com.alessandro.claymore.demo.modelproviders
 
+import com.alecarnevale.claymore.annotations.AutoBinds
 import com.alessandro.claymore.demo.models.Foo
 import com.alessandro.claymore.demo.models.FooImpl
+import dagger.hilt.android.components.ActivityComponent
 import javax.inject.Inject
 
+@AutoBinds(component = ActivityComponent::class)
 class FooProviderImpl @Inject constructor() : FooProvider {
   override fun get(): Foo {
     return FooImpl("FooProviderImpl")

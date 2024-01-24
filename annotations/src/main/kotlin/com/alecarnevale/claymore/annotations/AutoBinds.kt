2c0ef554ec6252e35d2f16d60ed561304e0e27d0
alecarnevale <alecarnevale.dev@gmail.com>
2024-01-24 18:19:49 +0100

    replace AutoBinds.intoSet with annotations field

--- annotations/src/main/kotlin/com/alecarnevale/claymore/annotations/AutoBinds.kt
+++ annotations/src/main/kotlin/com/alecarnevale/claymore/annotations/AutoBinds.kt
@@ -6,10 +6,11 @@ import kotlin.reflect.KClass
 /**
  * This annotation marks the implementation of an interface for which the hilt module will be generated.
  * @param component in which dagger component must be installed the module. If not specified, [SingletonComponent] will be used as default value.
+ * @param annotations which annotations attach in the generated binding function.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class AutoBinds(
   val component: KClass<*> = SingletonComponent::class,
-  val intoSet: Boolean = false,
+  val annotations: Array<KClass<*>> = [],
 )

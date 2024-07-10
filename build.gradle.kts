888af049fb615c02a24b2deabca78308d382dc44
Alessandro Carnevale <alecarnevale.dev@gmail.com>
2024-07-10 08:20:55 +0200

    Bump Kotlin 2.0 (#112)
    * bump kotlin and ksp 2.0
    
    * add kotlin-compose-compiler plugin
    
    * use kotlin-compose-compiler plugin
    
    * remove old compose-compiler version
    
    * update .gitignore
    
    * replace deprecated kotlinOptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.parcelize) apply false
+  alias(libs.plugins.kotlin.compose.compiler) apply false
   alias(libs.plugins.hilt) apply false
   alias(libs.plugins.ksp) apply false
   id("claymore-dependency") apply false
@@ -12,14 +13,14 @@ plugins {
 allprojects {
   pluginManager.withPlugin(rootProject.libs.plugins.kotlin.asProvider().get().pluginId) {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-      kotlinOptions {
+      compilerOptions {
         allWarningsAsErrors = true
       }
     }
   }
   pluginManager.withPlugin(rootProject.libs.plugins.kotlin.android.get().pluginId) {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-      kotlinOptions {
+      compilerOptions {
         allWarningsAsErrors = true
       }
     }
